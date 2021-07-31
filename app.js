b7f2c84a346df666cd3c188048177bca3bcbfd4c
Daniel Scherman <me@danielscherman.com>
2021-07-31 12:08:36 +0200

    Added type dependant background colors and hove effect.

--- app.js
+++ app.js
@@ -2,6 +2,26 @@
 const pkmnList = document.getElementById("pkmn_list");
 
 const pokeDex = {};
+const colours = {
+	normal: "#A8A77A",
+	fire: "#EE8130",
+	water: "#6390F0",
+	electric: "#F7D02C",
+	grass: "#7AC74C",
+	ice: "#96D9D6",
+	fighting: "#C22E28",
+	poison: "#A33EA1",
+	ground: "#E2BF65",
+	flying: "#A98FF3",
+	psychic: "#F95587",
+	bug: "#A6B91A",
+	rock: "#B6A136",
+	ghost: "#735797",
+	dragon: "#6F35FC",
+	dark: "#705746",
+	steel: "#B7B7CE",
+	fairy: "#D685AD",
+};
 
 //Fetch Pokemon
 async function fetchPokemon(id, callback) {
@@ -24,16 +44,24 @@ async function pokemonList(from, to) {
 function createCard(currentPkmn) {
 	const listItem = document.createElement("li"),
 		card = document.createElement("div");
+
+	const name = currentPkmn.name[0].toUpperCase() + currentPkmn.name.slice(1);
 	card.className = "pokemon_card";
 	card.dataset.pkmnId = currentPkmn.id;
 	card.innerHTML = `
-  <div class="image_wrapper">
+		<div class="hover-overlay"></div>
+  	<div class="image_wrapper">
 			<img
 				src="${currentPkmn.sprites.other["official-artwork"].front_default}"
 				alt="Balbasaur"
 			/>
 		</div>
+		<div class="pkmn-name">
+			<h1>${name}</h1>
+		</div>
   `;
+	card.style.backgroundColor = colours[currentPkmn.types[0].type.name];
+	console.log("color" + currentPkmn.types[0].type.name);
 	listItem.appendChild(card);
 	pkmnList.appendChild(listItem);
 }
